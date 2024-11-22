71a26df42b3356cdcad8150e76594aa6eefcfb5e
signor1 <emmanuelomemgboji@gmail.com>
2024-11-22 07:19:16 +0100

    feat: added a hook for fetching user token balances

--- src/hooks/useGetTokensAndBalances.js
+++ src/hooks/useGetTokensAndBalances.js
@@ -1,16 +1,63 @@
-import { useCallback, useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { token as tokenList } from "../data/tokenlist.json";
-import { Interface } from "ethers";
+import { Contract, Interface, isAddress, ZeroAddress } from "ethers";
+import erc20ABI from "../ABI/erc20.json";
+import multiCall2ABI from "../ABI/multicall2.json";
+import { JsonRpcProvider } from "ethers";
 
 const mainnetTokens = tokenList.filter((t) => t.chainId === 1);
-const useGetTokensAndBalances = () => {
+const useGetTokensAndBalances = (address) => {
   const [tokens, setTokens] = useState([]);
 
   const tokenAddresses = useMemo(() => mainnetTokens.map((t) => t.address), []);
 
-  const intface = useMemo(() => new Interface());
+  const intface = useMemo(() => new Interface(erc20ABI), []);
 
-  return useCallback(async (address) => {});
+  const userAddress = useMemo(
+    () => (isAddress(address) ? address : ZeroAddress),
+    [address]
+  );
+
+  const calls = useMemo(
+    () =>
+      tokenAddresses.map((address) => ({
+        target: address,
+        callData: intface.encodeFunctionData("balanceOf", [userAddress]),
+      })),
+    [intface, tokenAddresses, userAddress]
+  );
+
+  useEffect(() => {
+    (async () => {
+      const provider = new JsonRpcProvider(
+        import.meta.env.VITE_MAINNET_RPC_URL
+      );
+
+      const multiCallContract = new Contract(
+        import.meta.env.VITE_MULTICALL2_ADDRESS,
+        multiCall2ABI,
+        provider
+      );
+
+      // eslint-disable-next-line no-unused-vars
+      const [_, balancesResult] = await multiCallContract.aggregate.staticCall(
+        calls
+      );
+
+      const decodedBalances = balancesResult.map((result) =>
+        intface.decodeFunctionResult("balanceOf", result).toString()
+      );
+
+      const newTokensObj = mainnetTokens.map((token, index) => ({
+        ...token,
+        balance: decodedBalances[index],
+      }));
+
+      setTokens(newTokensObj);
+    })();
+  }, [calls, intface]);
+
+  return { tokens, userAddress };
 };
 
 export default useGetTokensAndBalances;
